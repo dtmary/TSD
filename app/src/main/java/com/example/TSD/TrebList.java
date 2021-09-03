8c803d3006b338e1a9e271e76940c05a0021bcd7
dtmary <dtmary@rambler.ru>
2021-09-03 10:45:55 +0300

    Android oracle

--- app/src/main/java/com/example/TSD/TrebList.java
+++ app/src/main/java/com/example/TSD/TrebList.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.myapplication111.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class TrebList extends AppCompatActivity {
 
@@ -12,5 +14,8 @@ public class TrebList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_treb_list);
+        ButterKnife.bind(this);
+
+
     }
 }
